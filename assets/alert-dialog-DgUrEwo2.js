3946566c6ad0776c2820c0dbeae0781ff1a508c5
ioursmid <dinamicasalfa@gmail.com>
2024-12-19 18:57:19 -0300

    Updates

---
+++ assets/alert-dialog-DgUrEwo2.js
@@ -0,0 +1,1 @@
+import{j as a,y as s,f as r,z as l}from"./index-YGpXjSOi.js";function i(t){const{leastDestructiveRef:o,...e}=t;return a.jsx(s,{...e,initialFocusRef:o})}const f=r((t,o)=>a.jsx(l,{ref:o,role:"alertdialog",...t}));export{i as A,f as a};
